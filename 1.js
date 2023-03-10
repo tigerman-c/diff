901d4b832232146fbe59ea989e8ad231c3dd2c64
陈凯文 <chenkaiwen@netseekgaming.com>
2023-03-10 11:52:55 +0800

    feat suc

--- 1.js
+++ 1.js
@@ -4,55 +4,67 @@ function getSequence(nums) {
     // 贪心：不管顺序，直接整最长递增子序列。
     // 二分： 提高性能。
     // 前驱节点： 记录真正的序列索引。
-    let arr = [];
+    let indexArr = []; // 存索引
     let preIndexArr = [];
     for (let i = 0; i < nums.length; i++) {
         const v = nums[i];
-        if (!arr.length) {
-            arr.push(v);
-            let preIndex = i - 1 >= 0 ? i - 1 : undefined;
-            preIndexArr.push(preIndex);
+        if (!indexArr.length) {
+            indexArr.push(i);
+            preIndexArr.push(indexArr[indexArr.length - 2]);
         }
         else {
-            if (v > arr[arr.length - 1]) {
+            const arrLast = nums[indexArr[indexArr.length - 1]];
+            if (v > arrLast) {
                 // 大于直接加
-                arr.push(v);
-                let preIndex = i - 1 >= 0 ? i - 1 : undefined;
-                preIndexArr.push(preIndex);
+                indexArr.push(i);
+                preIndexArr.push(indexArr[indexArr.length - 2]);
             }
-            if (v < arr[arr.length - 1]) {
+            if (v < arrLast) {
                 // 小于二分查找,找到比他大的最小值替换
-                let start = 0, end = arr.length - 1, middle;
-                while (end > start) {
+                let start = 0, end = indexArr.length - 1, middle = 0;
+                while (end >= start) {
                     middle = Math.floor((start + end) / 2);
-                    if (arr[middle] > v) {
-                        end = middle;
+                    if (nums[indexArr[middle]] > v) {
+                        end = middle - 1;
                     }
-                    else {
+                    else if (nums[indexArr[middle]] < v) {
                         start = middle + 1;
                     }
+                    else {
+                        break;
+                    }
                 }
-                if (middle !== undefined && arr[middle] > v) {
-                    // arr[middle] = v
-                    let preIndex = middle - 1 >= 0 ? middle - 1 : undefined;
-                    preIndexArr[i] = preIndex;
+                i === 5 && console.log(start, end, middle, indexArr + '');
+                if (nums[indexArr[start]] > v) {
+                    i === 5 && console.log(indexArr[start - 1], preIndexArr + '');
+                    indexArr[start] = i;
+                    preIndexArr.push(indexArr[start - 1]);
                 }
                 else {
-                    preIndexArr[i] = undefined;
+                    preIndexArr.push(undefined);
                 }
+            }
+            if (v === arrLast) {
+                preIndexArr.push(undefined);
             }
         }
         // if (!arr.length  arr[arr.length - 1])
     }
-    let res = [arr[arr.length - 1]];
-    console.log(arr, preIndexArr, 111);
-    for (let i = nums.indexOf(res[0]); i >= 0; i--) {
-        const preIndex = preIndexArr[i];
-        if (preIndex !== undefined && !res.includes(nums[preIndex])) {
-            res.unshift(nums[preIndex]);
-        }
+    let res = [nums[indexArr[indexArr.length - 1]]];
+    console.log(indexArr, preIndexArr, 111);
+    let p = preIndexArr[indexArr[indexArr.length - 1]];
+    while (p !== undefined) {
+        let v = nums[p];
+        res.unshift(v);
+        p = preIndexArr[p];
     }
+    // console.log(res, 'res')
     return res;
 }
-console.log(getSequence([1, 3, 2]));
-console.log(getSequence([2, 3, 1, 5, 6, 8, 7, 9, 4]));
+// console.log(getSequence([1, 1, 1,]))
+// console.log(getSequence([1, 3, 2, 3, 2])) //[1,2,3]
+// console.log(getSequence([2, 3, 1, 5, 6, 8, 7, 9, 4])) // [2,3,5,6,8,9]
+// console.log(getSequence([9, 2, 3])) // [2,3]  [u,]
+// console.log(getSequence([4, 10, 4, 3, 8, 9])) // [3,8,9]
+// console.log(getSequence([0, 1, 3, 2, 3])) // [0,1,2,3] 
+console.log(getSequence([3, 5, 6, 2, 5, 4, 19, 5, 6, 7, 12])); // [2,4,5,6,7,12]
